3de3b286f45faa9172b90c03c1c80bf72412e61b
Jordy Vieira <jordyvieira@hotmail.com>
2018-04-30 00:33:08 -0300

    Update convolution.py

--- trab2/convolution.py
+++ trab2/convolution.py
@@ -1,34 +1,43 @@
 import numpy as np
 from PIL import Image
 
-def conv_pixel(image, x, y, matrix, out):
+def conv_pixel(image, x, y, matrix, out, flag_y):
     xc = int(len(matrix[0]) / 2)
     yc = int(len(matrix) / 2)
     
     rt = 0.0
     gt = 0.0
     bt = 0.0
+    yt = 0.0
     
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             off_x = x + j - xc
             off_y = y + i - yc
             
-            rt += matrix[i, j]*image.getpixel((off_x, off_y))[0]
-            gt += matrix[i, j]*image.getpixel((off_x, off_y))[1]
-            bt += matrix[i, j]*image.getpixel((off_x, off_y))[2]
+            if not flag_y:
+                rt += matrix[i, j]*image.getpixel((off_x, off_y))[0]
+                gt += matrix[i, j]*image.getpixel((off_x, off_y))[1]
+                bt += matrix[i, j]*image.getpixel((off_x, off_y))[2]
+            else:
+                rt += matrix[i,j]*image[j,i,0]
     
-    rt = min(rt, 255)
-    gt = min(gt, 255)
-    bt = min(bt, 255)
+    if not flag_y:
+        rt = min(rt, 255)
+        gt = min(gt, 255)
+        bt = min(bt, 255)
     
-    rt = max(rt, 0)
-    gt = max(gt, 0)
-    bt = max(bt, 0)
+        rt = max(rt, 0)
+        gt = max(gt, 0)
+        bt = max(bt, 0)
     
-    out[y, x, 0] = rt
-    out[y, x, 1] = gt
-    out[y, x, 2] = bt
+        out[y, x, 0] = rt
+        out[y, x, 1] = gt
+        out[y, x, 2] = bt
+    else:
+        yt = min(yt, 255)
+        yt = max(yt, 0)
+        out = [y, x, 0] = yt
     
 def conv_image(image, matrix):
     out = np.zeros((image.height, image.width, 3), 'uint8')
