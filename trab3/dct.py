4d4558254ca6cb27ea2840e419d3c9b692070bc0
Jordy Vieira <jordyvieira@hotmail.com>
2019-03-13 20:48:20 -0300

    optimize

--- trab3/dct.py
+++ trab3/dct.py
@@ -4,35 +4,62 @@ import matplotlib.pyplot as plt
 import sys
 from math import cos, pi, sqrt
 from PIL import Image
+import time
 
-def dct(row):
+def calculate_cos(n, factor):
+    c = np.zeros((n,n))
+    s = sqrt(2/n)
+    for i in range(n):
+        for j in range(n):
+            c[i,j] = (cos((j + 0.5) * i * factor) * s)
+            
+    return c
+
+def calculate_inv_cos(n, factor):
+    c = np.zeros((n,n))
+    s = sqrt(2/n)
+    for i in range(n):
+        for j in range(1, n):
+            c[i,j] = (cos(factor * j * (i + 0.5)) * s)
+            
+    return c
+
+def dct(row, c):
+#def dct(row):
     N = len(row)
     result = []
     factor = pi / N
+    s = sqrt(2/N)
     
     for i in range(N):
         sum = 0.0
         for j in range(N):
-            sum += row[j] * cos((j + 0.5) * i * factor)
-        result.append(sum * sqrt(2 / N))
+            sum += row[j] * c[j,i]
+        result.append(sum * s)
 
     return result
+    #return row.dot(c)
 
 def dct2d(img):
     N = img.shape
+    c = calculate_cos(N[1], pi/N[1])
     rows = np.zeros_like(img).astype('float32')
     columns = np.zeros_like(img).astype('float32')
 
     for i in range(N[0]):
-        rows[i,:] = dct(img[i,:])
+        rows[i,:] = dct(img[i,:],c)
+    #rows = np.apply_along_axis(dct, 1, img, c)
     rows[0,:] /= sqrt(2)
 
+    c = calculate_cos(N[0], pi/N[0])
     for j in range(N[1]):
-        columns[:,j] = dct(rows[:,j])
+        columns[:,j] = dct(rows[:,j],c)
+    #columns = np.apply_along_axis(dct, 0, rows, c)
 
     return columns
 
-def inverse_dct(row):
+def inverse_dct(row, c):
+#def inverse_dct(row):
     N = len(row)
     result = []
     factor = pi / N
@@ -46,17 +73,24 @@ def inverse_dct(row):
         result.append(sum * sqrt(2/N))
 
     return result
+#    return row.dot(c)
 
 def inverse_dct2d(cosines):
     N = cosines.shape
     rows = np.zeros_like(cosines).astype('float32')
     columns = np.zeros_like(cosines).astype('float32')
 
+    c = calculate_inv_cos(N[0], pi/N[0])
     for i in range(N[1]):
-        rows[:,i] = inverse_dct(cosines[:,i])
-
+        rows[:,i] = inverse_dct(cosines[:,i],c)
+    
+    #rows = np.apply_along_axis(inverse_dct, 1, cosines, c)
+    #rows = np.clip(rows, 0, 255)
+    c = calculate_inv_cos(N[1], pi/N[1])
     for j in range(N[0]):
-        columns[j,:] = inverse_dct(rows[j,:])
+        columns[j,:] = inverse_dct(rows[j,:],c)
+    #columns = np.apply_along_axis(inverse_dct, 0, rows, c)
+    columns = np.clip(columns, 0, 255)
 
     return columns
 
@@ -95,16 +129,23 @@ if __name__ == "__main__":
     img = cv2.imread(filename)
     n = int(sys.argv[2])
     b,g,r = cv2.split(img)
+    start = time.time()
     b = dct2d(b)
     g = dct2d(g)
     r = dct2d(r)
+    print("dct conversion done")
+    
 
     b = get_n_cosines(b, n)
     g = get_n_cosines(g, n)
     r = get_n_cosines(r, n)
+    print("cosines selection done")
     b = inverse_dct2d(b)
     g = inverse_dct2d(g)
     r = inverse_dct2d(r)
+    print("inverse dct conversion done")
+    end = time.time()
+    print("time = " + str(end - start))
     x = cv2.merge((b,g,r))
     
     x = np.array(x).astype('uint8')
